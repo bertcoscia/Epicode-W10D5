2e4f448ba31b4c16126d80d536daa7ad0be6c078
A. Alberto Coscia <angeloalberto.coscia@gmail.com>
2024-07-05 13:31:30 +0200

    creates forecast layout

--- src/components/Forecast.jsx
+++ src/components/Forecast.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import { useEffect, useState } from "react";
-import { Button } from "react-bootstrap";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
+import { Col, Container, Row } from "react-bootstrap";
 
 const url = "https://api.openweathermap.org/data/2.5/weather?";
 const lat = "lat=";
@@ -12,11 +12,13 @@ const Forecast = () => {
   const [coordinates, setCoordinates] = useState(null);
   const params = useParams();
 
+  const [weather, setWeather] = useState(null);
+
   useEffect(() => {
     setCoordinates(params);
-    console.log(params);
-    console.log("coordinate", JSON.stringify(coordinates));
-    fetchForecast();
+    if (coordinates) {
+      fetchForecast();
+    }
   }, [coordinates]);
 
   const fetchForecast = () => {
@@ -30,13 +32,38 @@ const Forecast = () => {
       })
       .then(weather => {
         console.log("weather", weather);
+        setWeather(weather);
       });
   };
 
   return (
     <div>
-      <h1>Hello</h1>
-      <Button>Click</Button>
+      <Link to={"/"}>Back to Home</Link>
+      {weather ? (
+        <Container>
+          <h1>{weather.name}</h1>
+          <h2>{Math.round(weather.main.temp - 273)}&deg;</h2>
+          <small>H: {Math.round(weather.main.temp_max - 273)}&deg;</small>
+          <small>L: {Math.round(weather.main.temp_min - 273)}&deg;</small>
+          <p>{weather.weather[0].main}</p>
+          <Row>
+            <Col xs={12} md={6} className="card">
+              Wind: {weather.wind.speed}m/s
+            </Col>
+            <Col xs={12} md={6} className="card d-flex flex-column">
+              Temperature: {Math.round(weather.main.temp - 273)}&deg;
+            </Col>
+            <Col xs={12} md={6} className="card">
+              Humidity: {weather.main.humidity}%
+            </Col>
+            <Col xs={12} md={6} className="card">
+              Pressure: {weather.main.pressure}hPa
+            </Col>
+          </Row>
+        </Container>
+      ) : (
+        <h1>No data available</h1>
+      )}
     </div>
   );
 };
